17a7b7f45842da50baac0712c2eaec7a831fd363
Katerina Anurieva <kanureva.mail@gmail.com>
2024-04-19 15:43:47 +0300

    c

--- certific_generat/src/server.js
+++ certific_generat/src/server.js
@@ -5,10 +5,7 @@ const app = express()
 
 app.use(express.json())
 
-mongoose.connect('mongodb://localhost:27017/database', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect('mongodb://localhost:27017/database')
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
