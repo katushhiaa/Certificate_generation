818b9b7a0a7cd5cd926652e2d75e82e283fd092c
Katerina Anurieva <kanureva.mail@gmail.com>
2024-05-06 12:05:30 +0300

    delete

--- certific_generat/src/components/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h3>Home</h3>
-</template>
-
-<script>
- export default{
-        name: 'homeComp'
-    }
-</script>
--- certific_generat/src/components/StudentPage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h1>This is student page</h1> 
- </template>
- 
- <script>
- export default {
-     name: 'StudentComp'
- }
- </script>
