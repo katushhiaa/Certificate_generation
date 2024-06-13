d96dfdf44426136d791cae20cd8c5f84035f1665
Katerina Anurieva <kanureva.mail@gmail.com>
2024-06-13 03:23:58 +0300

    network

--- certific_generation/src/Network.js
+++ certific_generation/src/Network.js
@@ -6,8 +6,8 @@ const ERR_BAD_REQUEST = "ERR_BAD_REQUEST";
 
 class Network {
   constructor() {
-    //this.baseUrl = "https://certificate-generation-server.onrender.com";
-    this.baseUrl = "http://localhost:3001";
+    this.baseUrl = "https://certificate-generation-server.onrender.com";
+    //this.baseUrl = "http://localhost:3001";
   }
 
   signUp(data) {
@@ -19,7 +19,6 @@ class Network {
   }
 
   getAllStudents() {
-    //return axios.get(`${this.baseUrl}/students`, { headers: authHeader() });
     return this.authGet(`${this.baseUrl}/students`);
   }
 
@@ -30,7 +29,6 @@ class Network {
   }
 
   getCertData() {
-    //return axios.get(`${this.baseUrl}/getCertData`, { headers: authHeader() });
     return this.authGet(`${this.baseUrl}/getCertData`);
   }
 
@@ -68,10 +66,6 @@ class Network {
   }
 
   async getStudentCertificates(params) {
-    // return axios.get(`${this.baseUrl}/getStudentCertificates`, {
-    //   params,
-    //   headers: authHeader(),
-    // });
     return this.authGet(`${this.baseUrl}/getStudentCertificates`, {
       params,
     });
